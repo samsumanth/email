0402c7b9ea81768f797afcf2d2edc9b12a3a3cbe
samsumanth <81844736+samsumanth@users.noreply.github.com>
2021-05-22 15:46:25 +0530

    updated email.java

--- email.java
+++ email.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class Email_regex {
 	public static boolean isValid(String email) {
 		
-		String emailid = "[A-Z a-z 0-9]{3}[. _ + -][A-Z a-z 0-9]{3}+[@][a-z]{10}[.][a-z]{2-3}";
+		String emailid = "[A-Z a-z 0-9]{3}[. _ + -][A-Z a-z 0-9]{3}+[@][a-z]{10}[.][a-z]{2}.[a-z]{2}";
 		
 		Pattern p = Pattern.compile(emailid);
 		if(email == null) {
